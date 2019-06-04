1780959f9d07dca964e7a807dfd9c7c6bb500139
sshuair <sshuair@gmail.com>
2019-06-05 00:37:57 +0800

    添加地图爬虫脚本工具

---
+++ google-map-crawler/config.py
@@ -0,0 +1,6 @@
+defaults_provider={
+    'google-images-no-offset': 'https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}&scal=1',
+    'google-map-no-offset': 'https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}&scal=1',
+    'google-images-offset': 'http://www.google.cn/maps/vt?lyrs=s@189&gl=cn&x={x}&y={y}&z={z}&scal=1', ##google scale=2时可以可以返回512图片
+    'google-map-offset': 'http://www.google.cn/maps/vt?lyrs=m@189&gl=cn&x={x}&y={y}&z={z}&scal=1', 
+}
